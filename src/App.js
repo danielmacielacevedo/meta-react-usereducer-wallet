c2221c6a18fd64a8113c9062125aa3e2df9be3da
Daniel Maciel Acevedo <anndannma@outlook.com>
2023-03-08 21:14:28 -0600

    Agregué mi proyecto de Meta React al repositorio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useReducer } from 'react';
+
+const reducer = (state, action) => {
+  if (action.type === 'ride') return {money: state.money + 30};
+  if (action.type === 'fuel') return {money: state.money - 50};
+  return new Error();
+}
 
 function App() {
+  
+  const initialState = {money: 1000};
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='UseReducerContent'>
+        <h1>Wallet: ${state.money}</h1>
+        <div className='UseReducerButtons'>
+          <button onClick={() => dispatch({type: 'ride'})}>A new customer! +30</button>
+          <button onClick={() => dispatch({type: 'fuel'})}>Refil the tank! -50</button>
+        </div>
+      </div>
+
+      <div className="PoweredByDanielMacielAcevedo">
+        <p>Powered by Daniel Maciel Acevedo</p>
+        <a href="https://google.com/" ><span></span>ver codigo</a>
+      </div>
     </div>
   );
 }
